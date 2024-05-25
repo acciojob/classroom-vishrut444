e1df7ee6b6e1e18bc633c1f981bc4d010120674a
SNOW <visrut444@gmail.com>
2024-05-25 14:24:24 +0530

    solution-1

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -44,28 +44,36 @@ public class StudentController {
     @GetMapping("/get-student-by-name/{name}")
     public ResponseEntity<Student> getStudentByName(@PathVariable("name") String name){
         Student student = studentService.findStudent(name); // Assign student by calling service layer method
-
+        if (student == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-teacher-by-name/{name}")
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable("name") String name){
         Teacher teacher = studentService.findTeacher(name); // Assign student by calling service layer method
-
+        if (teacher == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-students-by-teacher-name/{teacher}")
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable("teacher") String teacher){
         List<String> students = studentService.findStudentsFromTeacher(teacher) ; // Assign list of student by calling service layer method
-
+        if(students.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-students")
     public ResponseEntity<List<String>> getAllStudents(){
         List<String> students = studentService.findAllStudents(); // Assign list of student by calling service layer method
-
+        if(students.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
